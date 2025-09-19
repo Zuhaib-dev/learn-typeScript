867db0592a2564bb4bcabe16512a2ec92fb5c878
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-09-19 15:12:25 +0530

    outsourced the axios for betterment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "./services/apiConfig";
 import React, { useEffect, useState } from "react";
 import Card from "./components/Card";
 
@@ -6,7 +6,7 @@ const App = () => {
   const [data, setdata] = useState([]);
   const getData = async () => {
     try {
-      const response = await axios.get("https://fakestoreapi.com/products");
+      const response = await axios.get("/products");
       setdata(response.data);
     } catch (error) {
       console.log(error);
