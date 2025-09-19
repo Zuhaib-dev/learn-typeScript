6af2a8d473346ab6d93a9e67503e9692d81b9130
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-09-19 09:59:22 +0530

    Learned 2 way binding again

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [username, setusername] = useState("");
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(username);
+    setusername("");
+  };
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <form onSubmit={submit}>
+        <input
+          value={username}
+          onChange={(e) => {
+            setusername(e.target.value);
+          }}
+          type="text"
+          placeholder="userName"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 
-export default App
+export default App;
