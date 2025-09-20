04d2e9663a58badb52a1948d6914a27aa9a2a568
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-09-20 14:52:15 +0530

    Another day trying to grind React now will grind the Context APi then will go to reduc toolkit

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,19 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { toast } from "react-toastify";
-import axios from "../services/apiConfig";
-import Card from '../components/Card'
+import { useEffect, useState } from "react";
+import Axios from "../services/apiConfig";
+import Card from "../components/Card";
 const Products = () => {
   const [data, setdata] = useState([]);
   const getData = async () => {
     try {
-      const response = await axios.get("/products");
+      const response = await Axios.get("/products");
       setdata(response.data);
-      setTimeout(() => {
-        toast.success("data Fetched ")
-      }, 1000);
     } catch (error) {
       console.log(error);
-      toast.error(error);
     }
   };
   useEffect(() => {
@@ -22,10 +17,20 @@ const Products = () => {
   return (
     <div>
       <>
-        <h1 className="flex justify-center mt-10">List Of All Products</h1>
-        <div className="flex flex-wrap px-10">
-          {data.map(function(item,idx){
-            return <Card key={idx} image={item.image} price={item.price} name={item.price} category={item.category} />
+        <h1 className="text-2xl flex justify-center mt-5">
+          List Of All Products
+        </h1>
+        <div className="flex flex-wrap px-17">
+          {data.map(function (elem, idx) {
+            return (
+              <Card
+                key={idx}
+                name={elem.title}
+                price={elem.price}
+                image={elem.image}
+                category={elem.category}
+              />
+            );
           })}
         </div>
       </>
