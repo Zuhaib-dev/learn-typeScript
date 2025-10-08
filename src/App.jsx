10b9c5cd03058f6f4127d5aa6141af795acfbf27
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-10-08 10:05:48 +0530

    Will see WHat else to do

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Mainroutes from './Routes/Mainroutes'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Mainroutes from "./Routes/Mainroutes";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Mainroutes />
-      
     </div>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
