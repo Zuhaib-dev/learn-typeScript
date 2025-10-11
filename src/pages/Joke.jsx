d3c26df0f71f562b35cd2382a8d449abcd49fb6f
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-10-11 08:41:33 +0530

    Done for today now will watch some tutorials of Reactjs to learn more and revise the leanred topics to get hand on React.js

--- src/pages/Joke.jsx
+++ src/pages/Joke.jsx
@@ -7,7 +7,7 @@ const Joke = () => {
   const getData = async () => {
     try {
       const response = await axios.get(
-        "https://official-joke-api.appspot.com/jokes/random/100"
+        "https://official-joke-api.appspot.com/jokes/random/250"
       );
       console.log(response.data);
 
@@ -29,9 +29,9 @@ const Joke = () => {
               key={idx}
               setup={elem.setup}
               delivery={elem.punchline}
-              category={elem.category}
+              category={elem.type}
               id={elem.id}
-              safe={elem.safe}
+              
             />
           );
         })}
